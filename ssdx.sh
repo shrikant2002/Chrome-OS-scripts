cbe196edbc5fb9a98f306c2638fc6666b237a0cf
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-04-30 17:36:55 +0530

    Update ssdx.sh
    partition size fixed

--- ssdx.sh
+++ ssdx.sh
@@ -15,13 +15,14 @@ sudo figlet -c Nimbalkar
 echo "Please enter partition name for Chrome OS multi boot install. For e.g. sda5";
 read partition
 sudo sfdisk -l /dev/$partition
+size=$(sudo fdisk -l /dev/$partition | awk '$1=="Disk" && $2 ~ /^\/dev\/.*/ {print int($3)}')
 while true; do
     read -p "Do you wish to format and install chrome os on $partition?(type yes to continue)" yn
     case $yn in
         [Yy]* ) echo "Ok, Proceeding installation on $partition";
                 mkdir -p ~/tmpmount;
                 sudo mount /dev/$partition ~/tmpmount;
-                sudo bash chromeos-install.sh -src rammus_recovery.bin -dst ~/tmpmount/chromos.img;
+                sudo bash chromeos-install.sh -src rammus_recovery.bin -dst ~/tmpmount/chromos.img -s $size;
                 sudo umount ~/tmpmount;
 				echo "Copy the above grub menu entry in grub customizer";
                 sudo grub-customizer;
