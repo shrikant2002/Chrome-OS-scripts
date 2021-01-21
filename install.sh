4829e227dd1107fc60f5616b1710806239d62bd5
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2021-01-21 15:28:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-# SUBSCRIBE to Kedar Nimbalkar on youtube for more such videos https://www.youtube.com/user/kedar123456889
+# SUBSCRIBE to How To Guys on youtube for more such videos https://www.youtube.com/c/HowToGuys
 sudo apt-get update
 sudo apt-get install figlet
 sudo apt-get install pv
 sudo apt-get install cgpt
 sudo figlet -c "SUBSCRIBE TO"
-sudo figlet -c Kedar
-sudo figlet -c Nimbalkar
-sudo echo https://www.youtube.com/user/kedar123456889
+sudo figlet -c "How To Guys"
+sudo echo https://www.youtube.com/c/HowToGuys
 sudo bash chromeos-install.sh -src rammus_recovery.bin -dst /dev/sda
